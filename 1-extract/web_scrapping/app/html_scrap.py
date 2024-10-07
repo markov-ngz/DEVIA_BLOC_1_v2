b63b16d66723e01e748a4a41fe487be224d14ab2
Garrodes <batuque77mlh@gmail.com>
2024-10-07 13:26:54 +0200

    Changed HTML extraction + fACTO

---
+++ 1-extract/web_scrapping/app/html_scrap.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+import requests
+
+
+class HTMLScrapper():
+        
+    @staticmethod
+    def get_content(url:str)->str:
+        """
+        Make a GET request to the URL and give the 
+        """
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                # log the status code retunred 
+                raise requests.HTTPError("Expected status code to be 200. Got instead %s".format(response.status_code))
+            return response.content
+        except Exception as e: 
+            # log the error
+            raise e
+        
+    
+    def selenium_scrapping(url:str, driver_path:str)->str:
+        """
+        From a URL and the path  to the Driver that will scrap the html
+        Return a str with all the html from the given page 
+        """
+
+        # driver monitor ( start / close )
+        service = Service(executable_path=driver_path)
+
+        options = webdriver.EdgeOptions()
+        options.add_argument("--headless=new")
+        
+        # driver window instantiation
+        driver = webdriver.Edge(service=service, options=options)
+
+
+
+        try : 
+            r = requests.get(url)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error:",errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting:",errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error:",errt)
+        except requests.exceptions.RequestException as err:
+            print ("Bad request:",err)
+        else:
+            # browser + GET to url 
+            driver.get(url)
+            # "html" -> str
+            html = driver.page_source
+            # wait 1 seconds 
+            WebDriverWait(driver, timeout=1) 
+
+            return html
+        finally:
+            driver.quit()
--- 1-extract/web_scrapping/app/main.py
+++ 1-extract/web_scrapping/app/main.py
@@ -1,9 +1,15 @@
-import html_parse
-import scrap
+from html_parse import HTMLParser
+from html_scrap import HTMLScrapper
 
-url_scrapped = "https://fr.wikiversity.org/wiki/Polonais/Vocabulaire/Se_pr%C3%A9senter"
 
-html_scrapped = scrap.web_scrapping(url_scrapped,"utils/msedgedriver.exe")
-raw_df_scrap = html_parse.parse_html(html_scrapped,url_scrapped)
+class WebScrapping():
 
-print(raw_df_scrap)
+    url = "https://fr.wikiversity.org/wiki/Polonais/Vocabulaire/Se_pr%C3%A9senter"
+    
+    def __init__(self) -> None:
+        html_scrapped = HTMLScrapper.get_content(self.url)
+        raw_df_scrap = HTMLScrapper.parse_html(html_scrapped,self.url)
+
+        print(raw_df_scrap)
+
+
