f2791c5c04692af08e2ecf0455badf7503ae661f
Garrodes <batuque77mlh@gmail.com>
2024-10-07 16:54:13 +0200

    FIX : fockerfile and loging module

--- 1-extract/web_scrapping/app/main.py
+++ 1-extract/web_scrapping/app/main.py
@@ -1,5 +1,8 @@
 from html_parse import HTMLParser
 from html_scrap import HTMLScrapper
+import logging
+import os 
+
 
 
 class WebScrapping():
@@ -7,6 +10,19 @@ class WebScrapping():
     url = "https://fr.wikiversity.org/wiki/Polonais/Vocabulaire/Se_pr%C3%A9senter"
     
     def __init__(self) -> None:
+        LOG_FILE = os.getenv("LOG_FILE")
+        if LOG_FILE == None:
+            raise ValueError("LOG_FILE environment variable not set")
+
+        with open(LOG_FILE,'a'):
+            pass
+
+        logging.basicConfig(filename=LOG_FILE,
+                            filemode='w',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.INFO)
+        logger = logging.getLogger(__name__)
         html_scrapped = HTMLScrapper.get_content(self.url)
         raw_df_scrap = HTMLScrapper.parse_html(html_scrapped,self.url)
 
