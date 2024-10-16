1ba297b087994c241ae33154a2d6d37fc3e02e74
markov-ngz <batuque77mlh@gmail.com>
2024-10-16 20:23:51 +0200

    ADD : loop over text to translate

--- 1-extract/api/src/main/java/com/devia/App.java
+++ 1-extract/api/src/main/java/com/devia/App.java
@@ -1,9 +1,11 @@
 package com.devia;
 
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.shaded.com.google.protobuf.Api;
 
@@ -15,32 +17,51 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        String bootstrap_server = System.getenv("KAFKA_BOOTSTRAP_SERVER"); ; 
-        String publish_topic = System.getenv("KAFKA_TOPIC"); ; 
 
-        // String url = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt" ; 
-        // IngestionProducer producer = new IngestionProducer(publish_topic,bootstrap_server);
+        // Kafka Topic whereabouts 
+        String bootstrap_server = System.getenv("KAFKA_BOOTSTRAP_SERVER"); 
+        String publish_topic = System.getenv("KAFKA_TOPIC"); 
+
+        // data that will be translated 
+        String[] item_to_translate = {"Salut!","Cours !","Courez !","Qui ?","Ça alors !","Au feu !","À l'aide !","Saute.","Ça suffit !","Stop !"} ; 
+
+        // API to call 
+        String url = "https://api-inference.huggingface.co/models/facebook/mbart-large-50-many-to-many-mmt" ; 
         
+        // Producer 
+        IngestionProducer producer = new IngestionProducer(publish_topic,bootstrap_server);
+
+        // Api handler 
+        ApiHandler api = new ApiHandler();
+
+        // OBject Mapper to de/serialize 
         ObjectMapper objectMapper = new ObjectMapper();
 
-        TranslationPayload map = new TranslationPayload("Bonjour");
-        // String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-        
-        // ApiHandler api = new ApiHandler();
-        // HttpResponse<String> response = api.sendPOST(url, requestBody);
-        // System.out.println(response.body());
-        String body = "[{\"translation_text\":\"Witaj!\"}]" ; 
+        // Iterate over the sentences 
+        for (String sentence : item_to_translate) {
 
-        TranslationResponse[] translated_texts = objectMapper.readValue(body, TranslationResponse[].class);
-        
-        Translation final_translation = new Translation(map.getInputs(), translated_texts[0].getTranslation_text());
+            TranslationPayload translation_payload = new TranslationPayload(sentence);
 
-        String translation_json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(final_translation);
+            String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(translation_payload);
+            
+            
+            HttpResponse<String> response = api.sendPOST(url, requestBody);
 
-        System.out.println(translation_json);
+            String body = response.body() ; 
 
-        // producer.writeMessage("123456", "random");
+            TranslationResponse[] translated_texts = objectMapper.readValue(body, TranslationResponse[].class);
+            
+            Translation final_translation = new Translation(translation_payload.getInputs(), translated_texts[0].getTranslation_text());
 
-        // producer.producer.close();
+            String translation_json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(final_translation);
+
+            // value is used as key in order to avoid duplicate to the topic 
+            producer.writeMessage(translation_json, translation_json);
+            
+            // delay to avoid rate limit from API 
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+        producer.producer.close();
     }
 }
