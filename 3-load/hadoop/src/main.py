f43547ae8b3073a566d53ff932122531f7d5707c
markov-ngz <batuque77mlh@gmail.com>
2024-11-16 11:33:55 +0100

    ADD select source columns

--- 3-load/hadoop/src/main.py
+++ 3-load/hadoop/src/main.py
@@ -19,9 +19,10 @@ class Main():
         spark = SparkHandler(self.app_name)
         spark.set_filesystem()
         df = spark.get_dataframes([settings.hdfs_folder],header=True,delimiter="\t", aggregate=True)
-        
+
         # 2. Get langues and sources 
         distinct_langs : list[Row] = df.select('lang_origin','lang_target').distinct().collect()
+        distinct_source : list[Row] = df.select('source_columns','source_type').distinct().collect()
 
         loader = TranslationLoader(settings.database_hostname, settings.database_port,settings.database_name ,settings.database_username, settings.database_password)
 
